24f130220c31c64b7f5e5adbffc10ffb09cf118b
RobinVCM <robin.malfait@viso-cor-mariae.be>
2022-05-16 08:45:30 +0200

    Add project files.

---
+++ asp0205222/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp0205222
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Response.Write("Klik op VERSTUUR om te versturen!");
+            }
+
+            else
+            {
+                Response.Write("Je hebt reeds op VERSTUUR geklikt!");
+            }
+        }
+
+        protected void Button(object sender, EventArgs e)
+        {
+
+
+
+        }
+    }
+}
